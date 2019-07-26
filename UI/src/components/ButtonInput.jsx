1c4c47ac2b04a52914dbb1248802ea035398306c
Emmanuel Ihemegbulam <emmanuelroic@gmail.com>
2019-07-26 10:28:24 +0100

    feat(login):implement user login
    - Create login page component
    - Implement login input validation
    - Implement auth POST request on successful validation
    
    [Delivers #167472140]

---
+++ UI/src/components/ButtonInput.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ButtonInput = (props) => {
+  const { name, defaultValue, action } = props;
+  return (
+    <div className="input-wrapper">
+      <p className="reg-form__text forgot-password" id="password-reset">
+        <Link className="reg-form__link m-sec-color" to="/signin">Forgot password?</Link>
+      </p>
+      <input
+        className='reg-form__button'
+        id={name}
+        type='button'
+        name={name}
+        defaultValue={defaultValue}
+        onClick={action}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default ButtonInput;
---
+++ UI/src/components/Input.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Input = (props) => {
+  const { name, title, value, inputType, handleChange, placeholder, error } = props;
+  return (
+    <div className='input-wrapper'>
+      <label className='reg-form__label' for={name}>{title}</label>
+      <input
+        className="reg-form__input"
+        id={name}
+        type={inputType}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        placeholder={placeholder}
+        {...props}
+      />
+      <span className='reg-form__span alert-message m-error'>{error}&nbsp;</span>
+    </div>
+  );
+};
+
+export default Input;
